disable mixed precision, fix tracking objective evaluation for log mesh

dno.py
@@ -88,6 +88,10 @@ def disable_gradient_checkpointing(model: nn.Module) -> nn.Module:
 
 class Trainer(BaseTrainer):
     def __init__(self, config):
+
+        num_gpus = torch.cuda.device_count()
+        assert num_gpus == 1, "Only supports single GPU"
+
         super().__init__(config)
 
         [ model[1].requires_grad_(False) for model in self.pipeline.models.items() ] # disable all gradients
@@ -131,7 +135,7 @@ class Trainer(BaseTrainer):
                         "intermediate_noise": ref_noise[1:],
                     }
                     cond = self.pipeline.get_cond([self.prompt])
-                    with torch.enable_grad(), self.accelerator.autocast():
+                    with torch.enable_grad():
                         ref_coords, ref_xs = self.pipeline.sample_sparse_structure(cond, 1, sparse_structure_sampler_params)
                     ref_meshes, ref_slats = self.generate_meshes_from_coords(cond, ref_coords)
                     ref_objective_value = self.objective_evaluator(ref_meshes)
@@ -175,24 +179,24 @@ class Trainer(BaseTrainer):
             else: # successfully optimize sample_i
                 all_meshes.append(meshes)
                 all_slats.append(slats)
-                all_objective_values.append(objective_values)
+                all_objective_values.append(torch.cat(objective_values))
             
             torch.cuda.empty_cache()
 
         # flip: (B, optimization_steps) -> (optimization_steps, B)
-        all_steps_objective_values = list(map(list, zip(*all_objective_values)))
+        all_objective_values = torch.stack(all_objective_values).T
         all_steps_meshes = list(map(list, zip(*all_meshes)))
         all_steps_slats = list(map(list, zip(*all_slats)))
 
         # log trajectory objective values
         for i in range(self.config.optimization_steps):
-            objective_values_i = torch.stack(all_steps_objective_values[i])[:,0]
-            self.log_objective_metrics(objective_values_i, objective_evaluations=self.config.total_num_samples * (self.config.batch_size+1) * i)
+            objective_evaluations = self.config.total_num_samples * (self.config.batch_size + 1) * (i + 1)
+            self.log_objective_metrics(all_objective_values[i], objective_evaluations=objective_evaluations)
             self.log_meshes(
                 all_steps_meshes[i],
                 all_steps_slats[i],
-                objective_values_i,
-                step=i,
+                all_objective_values[i],
+                objective_evaluations=objective_evaluations,
             )
 
         self.accelerator.log({"successful_samples": len(all_meshes)})

grpo.py
@@ -197,10 +197,10 @@ class Trainer(BaseTrainer):
                     t = training_kwargs_i["t"]
                     t_prev = training_kwargs_i["t_prev"]
 
-                    with torch.enable_grad(), self.accelerator.autocast():
+                    with torch.enable_grad():
                         prev_sample_mean = self.pipeline.sparse_structure_sampler.sample_once(self.pipeline.models['sparse_structure_flow_model'], training_batch["sample"][:,i], cond=training_batch["cond"][:,i], noise_level=0.7, **training_kwargs_i).pred_x_prev_mean
                         
-                    with self.accelerator.unwrap_model(self.pipeline.models['sparse_structure_flow_model']).disable_adapter(), torch.no_grad(), self.accelerator.autocast():
+                    with self.accelerator.unwrap_model(self.pipeline.models['sparse_structure_flow_model']).disable_adapter(), torch.no_grad():
                         prev_sample_mean_ref = self.pipeline.sparse_structure_sampler.sample_once(self.pipeline.models['sparse_structure_flow_model'], training_batch["sample"][:,i], cond=training_batch["cond"][:,i], noise_level=0.7, **training_kwargs_i).pred_x_prev_mean
 
                     curr_log_prob = self.pipeline.sparse_structure_sampler.calculate_log_prob(pred_x_prev_mean=prev_sample_mean, pred_x_prev=training_batch["prev_sample"][:,i], t=t, t_prev=t_prev, noise_level=0.7)
@@ -276,8 +276,9 @@ class Trainer(BaseTrainer):
 
         stage = "eval-noisy" if noisy else "eval"
 
-        self.log_meshes(all_meshes, all_slats, all_objective_values, epoch, stage=stage)
-        self.log_objective_metrics(all_objective_values, objective_evaluations=self.config.training_samples_per_epoch * epoch, stage=stage)
+        objective_evaluations = self.config.training_samples_per_epoch * epoch
+        self.log_meshes(all_meshes, all_slats, all_objective_values, objective_evaluations=objective_evaluations, stage=stage)
+        self.log_objective_metrics(all_objective_values, objective_evaluations=objective_evaluations, stage=stage)
         self.accelerator.wait_for_everyone()
 
 

nft.py
@@ -209,10 +209,10 @@ class Trainer(BaseTrainer):
                     with torch.no_grad(), use_adapter(self.accelerator.unwrap_model(self.pipeline.models['sparse_structure_flow_model']), "old"):
                         old_flow_pred = self.pipeline.models['sparse_structure_flow_model'](xt, t.expand(xt.shape[0]), cond)
 
-                    with torch.enable_grad(), self.accelerator.autocast():
+                    with torch.enable_grad():
                         curr_flow_pred = self.pipeline.models['sparse_structure_flow_model'](xt, t.expand(xt.shape[0]), cond)
                     
-                    with self.accelerator.unwrap_model(self.pipeline.models['sparse_structure_flow_model']).disable_adapter(), torch.no_grad(), self.accelerator.autocast():
+                    with self.accelerator.unwrap_model(self.pipeline.models['sparse_structure_flow_model']).disable_adapter(), torch.no_grad():
                         ref_flow_pred = self.pipeline.models['sparse_structure_flow_model'](xt, t.expand(xt.shape[0]), cond)
 
                     advantages = torch.clamp(training_batch["advantages"],-self.config.adv_clip_max,self.config.adv_clip_max)
@@ -274,8 +274,9 @@ class Trainer(BaseTrainer):
 
         all_objective_values = torch.cat(all_objective_values, dim=0)
 
-        self.log_meshes(all_meshes, all_slats, all_objective_values, step=epoch, stage="eval")
-        self.log_objective_metrics(all_objective_values, objective_evaluations=self.config.training_samples_per_epoch * epoch, stage="eval")
+        objective_evaluations = self.config.training_samples_per_epoch * epoch
+        self.log_meshes(all_meshes, all_slats, all_objective_values, objective_evaluations=objective_evaluations, stage="eval")
+        self.log_objective_metrics(all_objective_values, objective_evaluations=objective_evaluations, stage="eval")
         self.accelerator.wait_for_everyone()
 
 

optimize.py
@@ -202,8 +202,8 @@ class Trainer(BaseTrainer):
             all_objective_values.append(objective_values)
         all_objective_values = torch.cat(all_objective_values, dim=0)
         
-        self.log_meshes(all_meshes, all_slats, all_objective_values, step, stage="eval")
-        self.log_objective_metrics(all_objective_values, objective_evaluations=self.config.total_num_samples * step, stage="eval")
+        objective_evaluations = self.config.total_num_samples * step
+        self.log_objective_metrics(all_objective_values, objective_evaluations=objective_evaluations, stage="eval")
         self.save_parameters(step)
         self.accelerator.wait_for_everyone()
 

treeg.py
@@ -72,7 +72,7 @@ class Trainer(BaseTrainer):
             if torch.isfinite(objective_values_t).all():
                 self.log_objective_metrics(objective_values_t, objective_evaluations=self.config.total_num_samples * self.config.expansion_size * (t+1))
         
-        self.log_meshes(all_meshes,all_slats,all_log_trajectory_obj_values[:, -1],step=self.config.num_inference_steps-1,stage="eval")
+        self.log_meshes(all_meshes,all_slats,all_log_trajectory_obj_values[:, -1], objective_evaluations=self.config.total_num_samples * self.config.expansion_size * self.config.num_inference_steps, stage="final")
 
     @staticmethod
     def sample_once_treeg(
@@ -99,6 +99,9 @@ class Trainer(BaseTrainer):
         t_idx = np.argwhere(t_seq == t).item()
         t_prev_idx = t_idx + 1
         t_prev_prev_idx = t_idx + 2
+        cond_one = cond[0].unsqueeze(0)
+        cond_dict_one = external_self.pipeline.get_cond([external_self.prompt])
+        assert torch.allclose(cond, cond_one.expand_as(cond))
         # ----------- original code ----------- #
         pred_x_0, pred_eps, pred_v = self._get_model_prediction(model, x_t, t, cond, **kwargs)
         dt = t_prev - t
@@ -116,17 +119,15 @@ class Trainer(BaseTrainer):
             x_prev_candidates.append(x_prev_i)
             
             # determinisitcally sample once, decode and evaluate
-            try:
-                pred_sample_i = self.sample_once_original(model, x_prev_i, t_prev, t_seq[t_prev_prev_idx], cond, noise_level=0.0, **kwargs).pred_x_0 if t_prev_prev_idx < len(t_seq) else x_prev_i
-                coords = torch.argwhere(external_self.pipeline.models['sparse_structure_decoder'](pred_sample_i)>0)[:, [0, 2, 3, 4]].int()
-                cond_dict = external_self.pipeline.get_cond([external_self.prompt]*external_self.config.expansion_size)
-                meshes, slats = external_self.generate_meshes_from_coords(cond_dict, coords)
-                objective_values = external_self.objective_evaluator(meshes)
-                objective_values = torch.from_numpy(objective_values).to(x_t.device)
-            except Exception as e:
-                print(f"Exception {e} when decoding at {t_idx}-th timestep, assign inf objective values")
-                objective_values = torch.full((external_self.config.expansion_size,), float('inf'), device=x_t.device)
-            
+            objective_values = torch.full((external_self.config.expansion_size,), float('inf'), device=x_t.device)
+            for j, x_prev_ij in enumerate(x_prev_i):
+                try:
+                    pred_sample_ij = self.sample_once_original(model, x_prev_ij.unsqueeze(0), t_prev, t_seq[t_prev_prev_idx], cond_one, noise_level=0.0, **kwargs).pred_x_0 if t_prev_prev_idx < len(t_seq) else x_prev_ij.unsqueeze(0)
+                    coords = torch.argwhere(external_self.pipeline.models['sparse_structure_decoder'](pred_sample_ij)>0)[:, [0, 2, 3, 4]].int()
+                    meshes, slats = external_self.generate_meshes_from_coords(cond_dict_one, coords)
+                    objective_values[j] = torch.from_numpy(external_self.objective_evaluator(meshes)).to(x_t.device)
+                except Exception as e:
+                    print(f"Exception {e} when decoding at {t_idx}-th timestep, assign inf objective values")
             x_prev_candidates_obj_values.append(objective_values)
 
         # flatten